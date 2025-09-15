4ca8c36bf550891fc945934c91291780da5f9fba
yurats <ts.yurii@wwg.it>
2025-09-16 00:17:25 +0300

    feat: added buttons

---
+++ src/models/Buttons.ts
@@ -0,0 +1,41 @@
+import * as PIXI from "pixi.js";
+
+export class UIButton extends PIXI.Container {
+  private background: PIXI.Graphics;
+  private textLabel: PIXI.Text;
+
+  constructor(
+    text: string,
+    width: number,
+    height: number,
+    onClick: () => void,
+  ) {
+    super();
+
+    this.background = new PIXI.Graphics()
+      .roundRect(0, 0, width, height, 10)
+      .fill(0x3498db);
+    this.addChild(this.background);
+
+    this.textLabel = new PIXI.Text({
+      text,
+      style: {
+        fontFamily: "Arial",
+        fontSize: 14,
+        fill: 0xffffff,
+        align: "center",
+      },
+    });
+    this.textLabel.anchor.set(0.5);
+    this.textLabel.position.set(width / 2, height / 2);
+    this.addChild(this.textLabel);
+    this.eventMode = "static";
+    this.cursor = "pointer";
+
+    this.on("pointerdown", onClick);
+  }
+
+  public setText(text: string) {
+    this.textLabel.text = text;
+  }
+}
